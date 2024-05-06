a258289beaebf9a7df666302999e35ee71d721f6
Paula Njeru <Michel@DESKTOP-JJJOM2L>
2024-05-06 13:15:58 +0100

    Implemented a basic view model that includes properties of a tour name and a command to save the tour - using command(relaycommand.cs) as well as binding Modified application layout by moving models and view models into TourPlanner.UI

---
+++ TourPlanner.Commands/RelayCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Tour_planner.TourPlanner.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        private Action _execute;
+        private Func<bool> _canExecute;
+
+        public RelayCommand(Action execute, Func<bool> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException("execute");
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute();
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+    }
+
+}
---
+++ TourPlanner.UI/TourPlanner.ViewModels/TourViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+using Tour_planner.TourPlanner.Commands;
+
+
+namespace Tour_planner.TourPlanner.UI.TourPlanner.ViewModels
+{
+    public class TourViewModel : INotifyPropertyChanged
+        {
+            private string _tourName;
+
+            public string TourName
+            {
+                get => _tourName;
+                set
+                {
+                    if (_tourName != value)
+                    {
+                        _tourName = value;
+                        OnPropertyChanged(nameof(TourName));
+                    }
+                }
+            }
+
+            public ICommand SaveTourCommand { get; }
+
+            public TourViewModel()
+            {
+                SaveTourCommand = new RelayCommand(SaveTour);
+            }
+
+            private void SaveTour()
+            {
+                // Logic to save the tour could go here
+                // For demo purposes, we'll just simulate saving the data
+                System.Diagnostics.Debug.WriteLine($"Saving Tour: {TourName}");
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+            protected virtual void OnPropertyChanged(string propertyName)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+}
