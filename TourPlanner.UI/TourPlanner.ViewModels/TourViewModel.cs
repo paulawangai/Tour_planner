835d5f57093d477c2b886e5358b1229644a4f281
Paula Njeru <Michel@DESKTOP-JJJOM2L>
2024-05-06 13:35:00 +0100

    Extended view models with further data binding

--- TourPlanner.UI/TourPlanner.ViewModels/TourViewModel.cs
+++ TourPlanner.UI/TourPlanner.ViewModels/TourViewModel.cs
@@ -5,47 +5,87 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
 using System.Windows.Input;
+using System.Collections.ObjectModel;
 using Tour_planner.TourPlanner.Commands;
+using Tour_planner.TourPlanner.UI.TourPlanner.Models;
 
 
 namespace Tour_planner.TourPlanner.UI.TourPlanner.ViewModels
 {
-    public class TourViewModel : INotifyPropertyChanged
-        {
-            private string _tourName;
+     public class TourViewModel : INotifyPropertyChanged
+    {
+        private Tour _tour;
+        private string _newDestinationName;
 
-            public string TourName
+        public string? TourName
+        {
+            get => _tour.TourName;
+            set
             {
-                get => _tourName;
-                set
+                if (_tour.TourName != value)
                 {
-                    if (_tourName != value)
-                    {
-                        _tourName = value;
-                        OnPropertyChanged(nameof(TourName));
-                    }
+                    _tour.TourName = value;
+                    OnPropertyChanged(nameof(TourName));
                 }
             }
+        }
 
-            public ICommand SaveTourCommand { get; }
-
-            public TourViewModel()
+        public string? Description
+        {
+            get => _tour.Description;
+            set
             {
-                SaveTourCommand = new RelayCommand(SaveTour);
-            }
-
-            private void SaveTour()
-            {
-                // Logic to save the tour could go here
-                // For demo purposes, we'll just simulate saving the data
-                System.Diagnostics.Debug.WriteLine($"Saving Tour: {TourName}");
-            }
-
-            public event PropertyChangedEventHandler PropertyChanged;
-            protected virtual void OnPropertyChanged(string propertyName)
-            {
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                if (_tour.Description != value)
+                {
+                    _tour.Description = value;
+                    OnPropertyChanged(nameof(Description));
+                }
             }
         }
+
+        public ObservableCollection<string> Destinations { get; } = new ObservableCollection<string>();
+
+        public string NewDestinationName
+        {
+            get => _newDestinationName;
+            set
+            {
+                if (_newDestinationName != value)
+                {
+                    _newDestinationName = value;
+                    OnPropertyChanged(nameof(NewDestinationName));
+                }
+            }
+        }
+
+        public ICommand AddDestinationCommand { get; }
+        public ICommand SaveTourCommand { get; }
+
+        public TourViewModel()
+        {
+            _tour = new Tour();
+            AddDestinationCommand = new RelayCommand(AddDestination, CanAddDestination);
+            SaveTourCommand = new RelayCommand(SaveTour);
+        }
+
+        private bool CanAddDestination() => !string.IsNullOrEmpty(NewDestinationName);
+
+        private void AddDestination()
+        {
+            Destinations.Add(NewDestinationName);
+            NewDestinationName = string.Empty;
+        }
+
+        private void SaveTour()
+        {
+            // Save logic
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
 
 }
